2ab6dcfbf1f61e21ce8fbe10b7a944a1bee047a4
Beyrle Martin <martin.beyrle@stud.hs-kempten.de>
2020-04-10 18:13:53 +0200

    * ScoreEvaluator added * Word layer added
    Score Evaluator calculates how many words are in the box.

---
+++ Assets/Scripts/Score/ScoreEvaluator.cs
@@ -0,0 +1,97 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Words;
+
+public class ScoreEvaluator : MonoBehaviour
+{
+    public class ConnotationsCount
+    {
+        public int neutralCount;
+        public int insultingCount;
+        public int flatteringCount;
+        public int challengingCount;
+    }
+
+    [Tooltip("Edge collider sitting on a different GameObject, e.g. on a child.")]
+    public EdgeCollider2D edgeCollider;
+
+    // List of Colliders that are inside our object
+    private List<Collider2D> collidersInside = new List<Collider2D>();
+
+    private void Start()
+    {
+        // Filling the edge collider with the same points as our polygon collider. First point has to be added twice to receive a closed edge.
+
+        PolygonCollider2D polyCollider = GetComponent<PolygonCollider2D>();
+        
+        Vector2[] tmpVec2 = new Vector2[polyCollider.points.Length + 1];
+
+        for (int i = 0; i < polyCollider.points.Length; i++)
+        {
+            tmpVec2[i] = polyCollider.points[i];
+        }
+        tmpVec2[tmpVec2.Length - 1] = tmpVec2[0];
+
+
+        edgeCollider.points = tmpVec2;
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            CountBlocksInside();
+        }
+    }
+
+    public void CountBlocksInside()
+    {
+        ConnotationsCount connotationsCount = new ConnotationsCount();
+
+        // Go through all objects that are within my trigger
+        foreach (Collider2D c in collidersInside)
+        {
+            // If they are not on the edge of my trigger, increment connotation values
+            if (!Physics2D.IsTouching(c, edgeCollider))
+            {
+                Word word = c.GetComponent<Word>();
+
+                switch (word.connotation)
+                {
+                    case Connotation.Neutral:
+                        connotationsCount.neutralCount++;
+                        break;
+                    case Connotation.Insulting:
+                        connotationsCount.insultingCount++;
+                        break;
+                    case Connotation.Flattering:
+                        connotationsCount.flatteringCount++;
+                        break;
+                    case Connotation.Challenging:
+                        connotationsCount.challengingCount++;
+                        break;
+                }
+            }
+        }
+        
+        // TODO connotationsCount weitergeben
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.layer == LayerMask.NameToLayer("Word") && !collidersInside.Contains(collision))
+        {
+            collidersInside.Add(collision);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.layer == LayerMask.NameToLayer("Word") && collidersInside.Contains(collision))
+        {
+            collidersInside.Remove(collision);
+        }
+    }
+}
