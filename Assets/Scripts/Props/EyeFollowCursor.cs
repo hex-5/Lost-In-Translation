3418a5e29e850d8aa8af259bbd3f63f938803b54
singinwhale <singinwhale@protonmail.com>
2020-04-10 23:10:10 +0200

    eye is moving now

---
+++ Assets/Scripts/Props/EyeFollowCursor.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.U2D.Animation;
+
+[RequireComponent(typeof(SpriteSkin))]
+public class EyeFollowCursor : MonoBehaviour
+{
+
+    public Camera cam;
+    public int eyeBoneIndex = 1;
+    public float eyeMaxOffset = 1;
+
+    private SpriteSkin _skin;
+    private Vector3 _eyeLocalRestPosition;
+
+    void Start()
+    {
+        _skin = GetComponent<SpriteSkin>();
+        _eyeLocalRestPosition = _skin.boneTransforms[eyeBoneIndex].localPosition;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Transform eyeTransform = _skin.boneTransforms[eyeBoneIndex];
+        Vector3 cursorLocalPosition = eyeTransform.InverseTransformPoint(cam.ScreenToWorldPoint(Input.mousePosition));
+        eyeTransform.localPosition = _eyeLocalRestPosition + Vector3.ClampMagnitude(cursorLocalPosition, eyeMaxOffset);
+    }
+}
