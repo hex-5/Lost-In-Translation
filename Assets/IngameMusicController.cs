24399f4fdd1a31c7d57aa53ad662027c1c9638b2
singinwhale <singinwhale@protonmail.com>
2020-04-12 01:45:07 +0200

    Added ingame music

---
+++ Assets/IngameMusicController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering.UI;
+
+[RequireComponent(typeof(AudioSource))]
+public class IngameMusicController : MonoBehaviour
+{
+    private AudioSource _audioSource;
+
+    public GameObject managers;
+
+    [Serializable]
+    public struct AudioClipSection
+    {
+
+        public AudioClip clip;
+
+        public float from;
+    }
+
+    //public AudioClipSection section;
+    
+    public AudioClipSection[] normalSections;
+    public AudioClipSection[] losingSections;
+    public AudioClipSection[] intenseSections;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        _audioSource = GetComponent<AudioSource>();
+
+        managers.GetComponent<GameManager>().onNewSection += OnNewSection;
+    }
+
+    private void OnNewSection(GameManager manager, bool newgame)
+    {
+        if (newgame)
+        {
+            PlayAudioClipSection(normalSections[0]);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    private void PlayAudioClipSection(AudioClipSection clipSection)
+    {
+        _audioSource.clip = clipSection.clip;
+        _audioSource.PlayScheduled(clipSection.from);
+    }
+}
