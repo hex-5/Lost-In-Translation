aeaeed338999a7107d8beee85174deb8cd02575e
hex-5 <captainhex@t-online.de>
2020-04-12 00:26:51 +0200

    fixed lineendings and a bug in GameManager

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -122,6 +122,7 @@ public class GameManager : MonoBehaviour
     }
     public void UpdateRunningSection()
     {
+        if (gameEnded) return;
         currentSection.Countdown -= Time.deltaTime;
         if (currentSection.Countdown < 0)
         {
