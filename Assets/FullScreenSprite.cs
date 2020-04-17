269c4c66250d8e1d0210d58b21ab7df3f40f3c2e
Dominikus <dominikus.bertl@gmx.de>
2020-04-17 22:53:20 +0200

    Ok, really the last fix

---
+++ Assets/FullScreenSprite.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class FullScreenSprite : MonoBehaviour
+{
+    void Awake()
+    {
+        SpriteRenderer spriteRenderer = GetComponent<SpriteRenderer>();
+
+        float cameraHeight = Camera.main.orthographicSize * 2;
+        Vector2 cameraSize = new Vector2(Camera.main.aspect * cameraHeight, cameraHeight);
+        Vector2 spriteSize = spriteRenderer.sprite.bounds.size;
+
+        Vector2 scale = transform.localScale;
+        if (cameraSize.x >= cameraSize.y)
+        { // Landscape (or equal)
+            scale *= cameraSize.x / spriteSize.x;
+        }
+        else
+        { // Portrait
+            scale *= cameraSize.y / spriteSize.y;
+        }
+
+        transform.localScale = scale;
+    }
+}
